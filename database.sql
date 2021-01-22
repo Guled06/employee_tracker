0f14200550547e445ab5f4a26b1f20c41b3eeb3b
Guled Ahmed <akindnesstoremember@gmail.com>
2021-01-22 11:18:13 -0800

    fixed the databse.sql

--- database.sql
+++ database.sql
@@ -4,13 +4,12 @@ USE employeesDB;
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(45),
-  department_id INT(10),
   PRIMARY KEY (id)
 );
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(45) NOT NULL,
-  salary DECIMAL(10.3) NOT NULL,
+  salary DECIMAL(10,3) NOT NULL,
   department_id INT NULL,
   PRIMARY KEY (id)
 );
